e4b012233fd05e8bfcdbfc9df2405ae5c3871562
Arul <arul.jack@gmail.com>
2022-03-23 14:38:27 +0530

    testing2

--- app/src/main/java/com/signalstrength/MainActivity.kt
+++ app/src/main/java/com/signalstrength/MainActivity.kt
@@ -19,7 +19,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
-///hjkhkjhk
+///hjkhkjhkrrrrr
     private var gridLayout: GridLayout? = null
     val mainHandler = Handler(Looper.getMainLooper())
     override fun onCreate(savedInstanceState: Bundle?) {
